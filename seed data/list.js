26e70264299a3b3d5f04f905d4f1ebf05f581476
Bill Tran <bjtran1234@gmail.com>
2014-04-28 19:34:43 -0700

    heroku

---
+++ seed data/list.js
@@ -0,0 +1,54 @@
+db.restaurantcollection.remove({});
+
+db.restaurantcollection.insert({
+	"restaurant" : "Falafel Corner",
+	"restaurantitem" : [
+		{"item" : "Chicken Shish Kabob"},
+		{"item" : "Avocado Burger"},
+		{"item" : "Hawaiian"},
+		{"item" : "Double Cheeseburger"},
+		{"item" : "Tex-Mex Burger"},
+		{"item" : "Bacon Cheddar"},
+		{"item" : "Pepperoni pizza"},
+		{"item" : "Cheese pizza"}
+	    ]});
+
+db.restaurantcollection.insert({
+	"restaurant" : "zPizza",
+	"restaurantitem" : [
+		{"item" : "Penne with Meatballs"},
+		{"item" : "Calzone"},
+		{"item" : "Hot Meatball"},
+		{"item" : "Turkey Breast"},
+		{"item" : "Supersub"},
+		{"item" : "Meat Calzone"},
+		{"item" : "Chicken Penne"},
+		{"item" : "Ham and Pineapple"},
+		{"item" : "Ham Pizza"}
+        ]});
+
+db.restaurantcollection.insert({
+	"restaurant" : "Eastern Winds",
+	"restaurantitem" : [
+		{"item" : "Orange Chicken"},
+		{"item" : "Sesame Chicken"},
+		{"item" : "Chicken and Broc"},
+		{"item" : "Mongolian Beef"},
+		{"item" : "Kung Pao"},
+		{"item" : "Prawns with Broc"},
+		{"item" : "Tofu Prawns"}
+	    ]});
+
+db.restaurantcollection.insert({
+	"restaurant" : "King Taco",
+	"restaurantitem" : [
+		{"item" : "Carne Asada"},
+		{"item" : "Carnitas"},
+		{"item" : "Al Pastor"},
+		{"item" : "Lengua"},
+		{"item" : "Cabeza"},
+		{"item" : "Tripas"},
+		{"item" : "Molleja"},
+		{"item" : "Bean and Cheese"}
+	]
+});
